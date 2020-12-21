c7c0bb845351ce96913c47895110a33aafd3114b
L7-BAG <ll789@gmail.com>
2020-12-21 19:53:32 +0800

    登陆接口

--- src/main/java/com/l/controller/LoginController.java
+++ src/main/java/com/l/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.l.controller;
 
-import com.alibaba.fastjson.JSONObject;
+import com.l.pojo.User;
+import com.l.result.Result;
+import com.l.result.ResultFactory;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class LoginController {
-    @ApiOperation("测试前后端交互的接口")
-    @PostMapping(value = "/test")
-    public JSONObject test(@RequestBody JSONObject requestJSON) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("code", 200);
-        jsonObject.put("data", requestJSON);
-        return jsonObject;
+    @ApiOperation("登陆")
+    @PostMapping(value = "/login")
+    public Result test(@RequestBody User requestUser) {
+        String username = requestUser.getUsername();
+        String password = requestUser.getPassword();
+        if ("admin".equals(username) && "123456".equals(password)) {
+            return ResultFactory.buildSuccessResult(requestUser);
+        } else {
+            return ResultFactory.buildFailResult("帐号或者密码错误");
+        }
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
     }
 }
