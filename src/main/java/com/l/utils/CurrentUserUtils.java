93272eb3af1cc3eb9abfe3821a5056b378a34cb2
L7-BAG <ll789@gmail.com>
2021-02-20 16:37:34 +0800

    统一异常处理类

---
+++ src/main/java/com/l/utils/CurrentUserUtils.java
@@ -0,0 +1,37 @@
+package com.l.utils;
+
+import com.l.config.pojo.JwtUser;
+import com.l.service.impl.UserDetailsServiceImpl;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+/**
+ * 获取当前请求的用户工具类
+ *
+ * @author l
+ */
+@Component
+public class CurrentUserUtils {
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public CurrentUserUtils(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    public JwtUser getCurrentUser() {
+        return (JwtUser) userDetailsService.loadUserByUsername(getCurrentUserName());
+    }
+
+    /**
+     * TODO:由于在JWTAuthorizationFilter这个类注入UserDetailsServiceImpl一致失败，
+     * 导致无法正确查找到用户，所以存入Authentication的Principal为从 token 中取出的当前用户的姓名
+     */
+    private static String getCurrentUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() != null) {
+            return ((JwtUser) authentication.getPrincipal()).getUsername();
+        }
+        return null;
+    }
+}
